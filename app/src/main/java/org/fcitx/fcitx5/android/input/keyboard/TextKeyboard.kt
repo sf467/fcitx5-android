24eeba8d9ae56bd981d78f6b2fde40e5745200d6
sf467 <121373973+sf467@users.noreply.github.com>
2024-11-23 13:11:08 +0800

    Update TextKeyboard.kt
    尝试更改空格上的字2

--- app/src/main/java/org/fcitx/fcitx5/android/input/keyboard/TextKeyboard.kt
+++ app/src/main/java/org/fcitx/fcitx5/android/input/keyboard/TextKeyboard.kt
@@ -170,8 +170,8 @@ class TextKeyboard(
 
     override fun onInputMethodUpdate(ime: InputMethodEntry) {
         space.mainText.text = buildString {
-    //        append(ime.displayName)
-            ime.subMode.run { label.ifEmpty { name.ifEmpty { null } } }?.let { append(" ($it)") }
+            append(ime.displayName)
+//            ime.subMode.run { label.ifEmpty { name.ifEmpty { null } } }?.let { append(" ($it)") }
         }
         if (capsState != CapsState.None) {
             switchCapsState()
